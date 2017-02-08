51d14bae1e8a7c38fc9faf0705e00608be59a845
BP <oh_strawberry@yahoo.com>
2017-02-08 13:21:36 -0800

    UI design

---
+++ s04PartyRockApp/MainVC.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  s04PartyRockApp
+//
+//  Created by bernadette on 2/2/17.
+//  Copyright © 2017 Bernadette P. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    
+    }
+
+
+
+
+}
+
---
+++ s04PartyRockApp/PartyCell.swift
@@ -0,0 +1,25 @@
+//
+//  PartyCell.swift
+//  s04PartyRockApp
+//
+//  Created by bernadette on 2/8/17.
+//  Copyright © 2017 Bernadette P. All rights reserved.
+//
+
+import UIKit
+
+class PartyCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    
+        
+    }
+
+    func updateUI() {
+        
+    }
+
+
+}
